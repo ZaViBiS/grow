ce4133bab83835b7743fc84196d71b304bf23229
ygryk <ygryk>
2025-03-31 18:05:19 +0200

    async

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import _thread
+import asyncio
 import time
 import gc
 
-from microdot import Microdot
-from machine import Pin, I2C, PWM
+from microdot import Microdot, Response
+from machine import Pin, I2C
 from micropython_sht4x import sht4x
 
 from fan_control import FanControl
@@ -17,7 +17,6 @@ import tuny
 POINT_TEMP = 26
 POINT_HUM = 40
 
-
 # I2C
 i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=100_000)
 sht = sht4x.SHT4X(i2c)
@@ -27,7 +26,9 @@ ssd = Writer(o, tuny)
 out_fan = FanControl(1)
 out_fan.set_speed(0)
 
-server = Microdot()
+Response.default_content_type = "text/html"
+app = Microdot()
+
 smart = SmarkPlugContorl()
 
 
@@ -38,18 +39,18 @@ vpd_now = 0.0
 temp_avg, hum_avg, avg_vpd, fan_speed_avg = 0.0, 0.0, 0.0, 0.0
 
 
-def antidead_signal():
+async def antidead_signal():
     while True:
         for x in ["|", "/", "-", "\\"]:
             o.fill_rect(0, 50, 50, 20, 0)
             o.text(x, 0, 50, 1)
             o.show()
-            time.sleep(0.8)
+            await asyncio.sleep(0.8)
 
 
-@server.route("/")
-def main_page(request):
-    server.send(
+@app.route("/")
+async def index(request):
+    return Response(
         utils.html_load(
             temp_now,
             temp_avg,
@@ -64,54 +65,60 @@ def main_page(request):
     )
 
 
-_thread.start_new_thread(server.run, ())
+async def main_loop():
+    global temp_now, hum_now, vpd_now, temp_avg, hum_avg, avg_vpd, fan_speed_avg
+    stat = utils.Statistics()
+    while True:
+        temp_now, hum_now = sht.measurements
+        vpd_now = utils.vpd_calculator(temp_now, hum_now)
 
-stat = utils.Statistics()
-_thread.start_new_thread(antidead_signal, ())
-
-while True:
-    temp_now, hum_now = sht.measurements
-    vpd_now = utils.vpd_calculator(temp_now, hum_now)
-
-    stat.data.append(
-        (
-            temp_now,
-            hum_now,
-            time.time(),
-            utils.vpd_calculator(temp_now, hum_now),
-            out_fan.fan_speed,
+        stat.data.append(
+            (
+                temp_now,
+                hum_now,
+                time.time(),
+                utils.vpd_calculator(temp_now, hum_now),
+                out_fan.fan_speed,
+            )
         )
-    )
 
-    start = time.ticks_ms()
-    if temp_now > POINT_TEMP + 1:
-        out_fan.set_speed(out_fan.fan_speed + 1)
-    elif temp_now < POINT_TEMP - 1:
-        out_fan.set_speed(out_fan.fan_speed - 1)
+        # if int(hum_now) == POINT_HUM and not smart.last_status:
+        #     smart.change_condition(False)
+        # else:
+        #     if hum_now > POINT_HUM + 5:
+        #         smart.change_condition(True)
+        #     elif hum_now < POINT_HUM - 5:
+        #         smart.change_condition(False)
 
-    # if int(hum_now) == POINT_HUM and not smart.last_status:
-    #     smart.change_condition(False)
-    # else:
-    #     if hum_now > POINT_HUM + 5:
-    #         smart.change_condition(True)
-    #     elif hum_now < POINT_HUM - 5:
-    #         smart.change_condition(False)
+        if temp_now > POINT_TEMP + 1:
+            out_fan.set_speed(out_fan.fan_speed + 1)
+        elif temp_now < POINT_TEMP - 1:
+            out_fan.set_speed(out_fan.fan_speed - 1)
 
-    utils.log(
-        f"temp: {temp_now:.2f}C, hum: {hum_now:.2f}% | out_fan speed: {out_fan.fan_speed}"
-    )
-    temp_avg, hum_avg, vpd_avg, avg_fan_speed = stat.statistics_for_24h()
-    o.fill(0)
-    ssd.set_textpos(o, 0, 0)
-    ssd.printstring(f"TEMP/avg: {temp_now:.2f}C | {temp_avg:.2f}C\n")
-    ssd.printstring(f"HUM/avg:  {hum_now:.2f}% | {hum_avg:.2f}%\n")
-    ssd.printstring(f"OUT fan speed: {out_fan.fan_speed}% | {avg_fan_speed}%\n")
-    ssd.printstring(f"VPD: {vpd_now:.2f} avg: {vpd_avg:.2f}\n")
-    ssd.printstring(f"ram free: {gc.mem_free()} bytes\n")
-    o.show()
+        utils.log(
+            f"temp: {temp_now:.2f}C, hum: {hum_now:.2f}% | out_fan speed: {out_fan.fan_speed}"
+        )
+        temp_avg, hum_avg, avg_vpd, fan_speed_avg = stat.statistics_for_24h()
+        o.fill(0)
+        ssd.set_textpos(o, 0, 0)
+        ssd.printstring(f"TEMP/avg: {temp_now:.2f}C | {temp_avg:.2f}C\n")
+        ssd.printstring(f"HUM/avg:  {hum_now:.2f}% | {hum_avg:.2f}%\n")
+        ssd.printstring(f"OUT fan speed: {out_fan.fan_speed}% | {fan_speed_avg}%\n")
+        ssd.printstring(f"VPD: {vpd_now:.2f} avg: {avg_vpd:.2f}\n")
+        ssd.printstring(f"ram free: {gc.mem_free()} bytes\n")
+        o.show()
+        gc.collect()
 
-    gc.collect()
+        await asyncio.sleep(2.5 * 60)
 
-    delay_time = (2.5 * 60) - ((time.ticks_ms() - start) / 1000)
-    if delay_time > 0:
-        time.sleep(delay_time)
+
+async def main():
+    server = asyncio.create_task(app.start_server())
+
+    asyncio.create_task(antidead_signal())
+    asyncio.create_task(main_loop())
+
+    await server
+
+
+asyncio.run(main())
