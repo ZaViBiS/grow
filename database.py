812eec7aae1ac6e7fc79a1c3a8e69015b7ddc352
ygryk <ygryk>
2025-04-02 21:20:01 +0200

    init

---
+++ database.py
@@ -0,0 +1,16 @@
+import firebase
+
+URL = "https://grow-192db-default-rtdb.europe-west1.firebasedatabase.app/"
+
+
+class Database:
+    def __init__(self) -> None:
+        firebase.setURL(URL)
+
+    async def put(self, time: int, temp: float, hum: float, fan_speed: int) -> None:
+        data = {
+            "temp": temp,
+            "hum": hum,
+            "fan_speed": fan_speed,
+        }
+        firebase.put(f"/data/{time + 946684800}", data)
