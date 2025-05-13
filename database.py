5b1fa769a37efad27a14c3bf96220885b9025060
ygryk <ygryk>
2025-05-13 14:03:31 +0200

    add pid and 20 sec to 60 sec

--- database.py
+++ database.py
@@ -7,7 +7,16 @@ class Database:
     def __init__(self) -> None:
         pass
 
-    def put(self, data: dict) -> bool:
+    def put(
+        self, time: int, temp: float, hum: float, fan_speed: int, vpd: float
+    ) -> bool:
+        data = {
+            "time": time,
+            "temp": temp,
+            "hum": hum,
+            "fan_speed": fan_speed,
+            "vpd": vpd,
+        }
         r = requests.post(URL, json=data, timeout=20)
         print(r.status_code)
 
