9224cd6c7b7db8ecc10c333fe14c2274ed47c0fd
ygryk <ygryk>
2025-05-14 04:33:04 +0200

    -переніс відправку даних в окремий поітк\n-kp змінив на 5, було 10\n-тепер оновлення швидкості відбуваєтся кожні 2 секунди

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import _thread
 import time
 import gc
@@ -11,61 +10,69 @@ from database import Database
 import utils
 
 smd = utils.SendMissedData()
+db = Database()
+points = utils.Points()
+out_fan = FanControl(1)
+out_fan.set_speed(0)
+
+temp, hum = 0.0, 0.0
+points.update_data_in_class()
+
+
+def sender():
+    """thread that send data every 60 second"""
+    try:
+        while True:
+            start = time.ticks_ms()
+            unixtime = int(utils.get_unix_time_now(time.time()))
+
+            if not db.put(
+                time=unixtime,
+                temp=temp,
+                hum=hum,
+                fan_speed=out_fan.fan_speed,
+                vpd=utils.vpd_calculator(temp, hum),
+            ):
+                utils.log("Exception: проблема при відпраці даних")
+                raise
+
+            time.sleep(60 - time.ticks_diff(time.ticks_ms(), start))
+
+    except Exception as e:
+        utils.log(f"error in data sending: {e}")
+        smd.queue.append(
+            {
+                "time": int(unixtime),
+                "temp": temp,
+                "hum": hum,
+                "fan_speed": out_fan.fan_speed,
+                "vpd": utils.vpd_calculator(temp, hum),
+            }
+        )
+        if not sta_if.isconnected():
+            reset()
 
 
 def main_loop():
     # I2C
     i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=100000)
     sht = sht4x.SHT4X(i2c)
+    pid = utils.PID(kp=5, ki=0.2, kd=2, setpoint=points.POINT_TEMP)
 
-    out_fan = FanControl(1)
-    out_fan.set_speed(0)
-
-    points = utils.Points()
-    db = Database()
-
-    points.update_data_in_class()
-
-    pid = utils.PID(kp=10, ki=0.2, kd=2, setpoint=points.POINT_TEMP)
     while True:
-        start = time.time()
-        unixtime = utils.get_unix_time_now(start)
+        start = time.ticks_ms()
         temp, hum = sht.measurements
-        vpd = utils.vpd_calculator(temp, hum)
-
-        try:
-            if not db.put(
-                time=int(unixtime),
-                temp=temp,
-                hum=hum,
-                fan_speed=out_fan.fan_speed,
-                vpd=vpd,
-            ):
-                raise
-
-        except Exception as e:
-            utils.log(f"error in data sending: {e}")
-            smd.queue.append(
-                {
-                    "time": int(unixtime),
-                    "temp": temp,
-                    "hum": hum,
-                    "fan_speed": out_fan.fan_speed,
-                    "vpd": vpd,
-                }
-            )
-            if not sta_if.isconnected():
-                reset()
 
         speed = pid.compute(temp)
         out_fan.set_speed(int(speed))
 
         gc.collect()
 
-        time.sleep(60 - (time.time() - start))
+        time.sleep(2 - time.ticks_diff(time.ticks_ms(), start))
 
 
 try:
+    _thread.start_new_thread(sender, ())
     _thread.start_new_thread(smd.send_missed_data, ())
     main_loop()
 except Exception as e:
